5b199e4c4c1d5b3edc89451d4130135d39660b47
Kirilldid <kirilldid@gmail.com>
2024-09-24 00:04:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    const display = document.getElementById("display");
+    
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Ошибка";
+        setTimeout(clearDisplay, 1500); // Очистка дисплея через 1.5 секунды
+    }
+}
